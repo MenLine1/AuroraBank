09ee02eb6d7bd8f3e13c99c14620a49b749907f3
MenLine1 <mmartyna790@gmail.com>
2024-07-05 15:18:07 +0200

    implementation of project structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function RootLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <main>
+            SIDEBAR
+            {children}
+        </main>
+    );
+}
